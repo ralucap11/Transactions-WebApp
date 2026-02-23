6c507d1ca312dee9b332a812013b3f67478f2818
Raluca <raluca.paraschiv@student.upt.ro>
2026-02-23 15:01:18 +0200

    added time filters

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,17 @@ function App() {
   const [newValue, setNewValue] = useState<number | string>("");
   const [newDate, setNewDate] = useState("");
   const [expandedId, setExpandedId] = useState<number | null>(null);
+  const [currentType, setCurrentType] = useState("all");
+ const [currentTime, setCurrentTime] = useState("all");
 
-  const fetchTasks = async () => {
+  const fetchTasks = async (type = currentType, time = currentTime) => {
     try {
-      const response = await axios.get(API_URL);
+      setLoading(true);
+      const response = await axios.get(`${API_URL}?type=${type}&dateFilter=${time}`);     
       setTasks(response.data);
+
+      setCurrentType(type);
+    setCurrentTime(time);
     } catch (error) {
       console.error("error at taking tasks", error);
     } finally {
@@ -78,7 +84,24 @@ function App() {
         newValue={newValue} setNewValue={setNewValue}
         onSubmit={handleSubmission}
       />
-
+          <div style={{ marginBottom: "20px", display: "flex", gap: "10px" }}>
+  <button onClick={() => fetchTasks("all")} className="filter-btn btn-all">
+    All
+  </button>
+  <button onClick={() => fetchTasks("income", currentTime)} className="filter-btn btn-income">
+    Income
+  </button>
+  <button onClick={() => fetchTasks("expense")} className="filter-btn btn-expense">
+    Expenses
+  </button>
+  <select onChange={(e) => fetchTasks(currentType, e.target.value)} className="date-filter">
+  <option value="all">All Time</option>
+  <option value="month">Last Month</option>
+  <option value="3months">Last 3 Months</option>
+  <option value="6months">Last 6 Months</option>
+  <option value="year">Last Year</option>
+</select>
+</div>
       <ul>
         {tasks.length === 0 && <li>No transactions found.</li>}
         {tasks.map((task) => (
